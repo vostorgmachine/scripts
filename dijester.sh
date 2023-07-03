f39c697670a2f0d0ad932fb1d9f522d1b7ca3789
vostorg <106368478+vostorgmachine@users.noreply.github.com>
2023-07-03 15:25:07 +0600

    Add files via upload

--- dijester.sh
+++ dijester.sh
@@ -14,6 +14,8 @@
 
 # Скопировать все .md в отдельную директорию для дальнейшей обработки 
 
+mkdir ../source/dijest
+
 find . -type f -name "*.md" | xargs cp -vt ../source/dijest
 
 cd ../source/dijest/
@@ -22,14 +24,24 @@ echo ""
 pwd
 echo ""
 
-mkdir order repression war economy other
+mkdir stats order repression war economy other
 
 # # grep-секция
 
+grep -rlP '^(?=.*Теги)(?=.*Статистика)' | xargs mv -vt stats/
 grep -rlP '^(?=.*Теги)(?=.*Закон)' | xargs mv -vt order/
 grep -rlP '^(?=.*Теги)(?=.*Репресс)' | xargs mv -vt repression/
 grep -rlP '^(?=.*Теги)(?=.*Войн)' | xargs mv -vt war/
 grep -rlP '^(?=.*Теги)(?=.*Экономи)' | xargs mv -vt economy/
+
+cd stats/
+echo "# Статистика" >> stats.md
+echo "" >> stats.md
+cat *.md >> stats.md
+echo "" >> stats.md
+echo "-----" >> stats.md
+echo "" >> stats.md
+cd ../
 
 cd order/
 echo "# Закон" >> order.md
@@ -37,6 +49,7 @@ echo "" >> order.md
 cat *.md >> order.md
 echo "" >> order.md
 echo "-----" >> order.md
+echo "" >> order.md
 cd ../
 
 cd repression
@@ -45,6 +58,7 @@ echo "" >> repression.md
 cat *.md >> repression.md
 echo "" >> repression.md
 echo "-----" >> repression.md
+echo "" >> repression.md
 cd ../
 
 cd war
@@ -53,6 +67,7 @@ echo "" >> war.md
 cat *.md >> war.md
 echo "" >> war.md
 echo "-----" >> war.md
+echo "" >> war.md
 cd ../
 
 cd economy
@@ -61,9 +76,26 @@ echo "" >> economy.md
 cat *.md >> economy.md
 echo "" >> economy.md
 echo "-----" >> economy.md
+echo "" >> economy.md
 cd ../
 
 mv *.md other/
+cd other/
+echo "# Прочее" >> other.md
+echo "" >> other.md
+cat *.md >> other.md
+cd ../
+
+echo ""
+pwd
+echo ""
+
+cat stats/stats.md >> dijest.md
+cat order/order.md >> dijest.md
+cat repression/repression.md >> dijest.md
+cat war/war.md >> dijest.md
+cat economy/economy.md >> dijest.md
+cat other/other.md >> dijest.md
 
 echo ""
 echo "done!"
